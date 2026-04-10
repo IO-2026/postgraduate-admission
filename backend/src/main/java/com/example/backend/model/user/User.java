ec2f27ab7d5aa0061acd91127ed5f09273391a2a
Grzegorz Rybiński <grrybinski@gmail.com>
2026-04-10 23:18:20 +0200

    IO-9 add jwt secret

--- backend/src/main/java/com/example/backend/model/user/User.java
+++ backend/src/main/java/com/example/backend/model/user/User.java
@@ -70,4 +70,15 @@ public class User implements UserDetails {
     public boolean isEnabled() {
         return true;
     }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "name='" + name + '\'' +
+                ", surname='" + surname + '\'' +
+                ", email='" + email + '\'' +
+                ", role=" + role +
+                '}';
+    }
+
 }
