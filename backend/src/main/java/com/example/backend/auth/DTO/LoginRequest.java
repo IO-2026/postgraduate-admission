b7cc50c4d48a5e205dbb2db611ad8cc3a57e7d4d
Grzegorz Rybiński <grrybinski@gmail.com>
2026-04-13 15:33:25 +0200

    IO-6 satisfy checkstyle rules

--- backend/src/main/java/com/example/backend/auth/DTO/LoginRequest.java
+++ backend/src/main/java/com/example/backend/auth/DTO/LoginRequest.java
@@ -1,6 +1,5 @@
 package com.example.backend.auth.DTO;
 
-import jakarta.validation.constraints.Size;
 import lombok.Data;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
