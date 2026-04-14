04d9bcc4c3dfa31e6d002aa29feab4a42b5ccbc7
Jan Martowicz <martowiczjan@gmail.com>
2026-04-14 18:27:17 +0200

    feat(IO-15): fixing checkstyle x2

--- backend/src/main/java/com/example/backend/model/application/Application.java
+++ backend/src/main/java/com/example/backend/model/application/Application.java
@@ -2,7 +2,16 @@ package com.example.backend.model.application;
 
 
 import com.example.backend.model.user.User;
-import jakarta.persistence.*;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.Id;
+import jakarta.persistence.FetchType;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
