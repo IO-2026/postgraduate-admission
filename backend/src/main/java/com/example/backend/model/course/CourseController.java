7dab5c88c1f8993c86b2efd1b7f0981c4e4ecc74
Jan Martowicz <martowiczjan@gmail.com>
2026-04-30 00:40:04 +0200

    deleted e.printStackTrace()

--- backend/src/main/java/com/example/backend/model/course/CourseController.java
+++ backend/src/main/java/com/example/backend/model/course/CourseController.java
@@ -1,6 +1,7 @@
 package com.example.backend.model.course;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+
 @RestController
 @RequestMapping("/api")
 @RequiredArgsConstructor
@@ -31,7 +33,6 @@ public class CourseController {
             CourseDTO savedCourse = courseService.saveCourse(courseDTO);
             return ResponseEntity.status(HttpStatus.CREATED).body(savedCourse);
         } catch (Exception e) {
-            e.printStackTrace();
             return ResponseEntity.status(500).body("Error: " + e.getMessage() + (e.getCause() != null ? " Cause: " + e.getCause().getMessage() : ""));
         }
     }
@@ -42,7 +43,6 @@ public class CourseController {
             courseService.deleteCourse(id);
             return ResponseEntity.ok().build();
         } catch (Exception e) {
-            e.printStackTrace();
             return ResponseEntity.status(500).body("Error: " + e.getMessage());
         }
     }
@@ -53,7 +53,6 @@ public class CourseController {
             CourseDTO updatedCourse = courseService.updateCourse(id, courseDTO);
             return ResponseEntity.ok(updatedCourse);
         } catch (Exception e) {
-            e.printStackTrace();
             return ResponseEntity.status(500).body("Error: " + e.getMessage());
         }
     }
