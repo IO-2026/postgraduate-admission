49002af2b065f9122a1e866236729af52414e949
Jan Martowicz <martowiczjan@gmail.com>
2026-05-08 14:32:03 +0200

    moved declaration endpoint to ApplicationController since it is involved with Applications

--- backend/src/main/java/com/example/backend/model/application/ApplicationController.java
+++ backend/src/main/java/com/example/backend/model/application/ApplicationController.java
@@ -21,9 +21,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 
 
 @RestController
