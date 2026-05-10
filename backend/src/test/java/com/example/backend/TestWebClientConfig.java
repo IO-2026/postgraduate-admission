142ba693cddbd1501a83e3e8d9e36ce405caa529
Brtosz <btokarz@student.agh.edu.pl>
2026-05-10 02:32:44 +0200

    fixed tests

--- backend/src/test/java/com/example/backend/TestWebClientConfig.java
+++ backend/src/test/java/com/example/backend/TestWebClientConfig.java
@@ -4,16 +4,10 @@ import com.example.backend.storage.SupabaseStorageService;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Primary;
-import org.springframework.web.reactive.function.client.WebClient;
 import org.mockito.Mockito;
 
 @TestConfiguration
 public class TestWebClientConfig {
-    @Bean
-    public WebClient.Builder webClientBuilder() {
-        return WebClient.builder();
-    }
-
     @Bean
     @Primary
     public SupabaseStorageService mockSupabaseStorageService() {
