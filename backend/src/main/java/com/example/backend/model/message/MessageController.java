8d64f9728db835719e27f69a3e476f60c4172d13
Grzegorz Rybiński <grrybinski@gmail.com>
2026-05-20 12:17:41 +0200

    IO-72: endpoint preAuth

--- backend/src/main/java/com/example/backend/model/message/MessageController.java
+++ backend/src/main/java/com/example/backend/model/message/MessageController.java
@@ -8,6 +8,7 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -64,6 +65,7 @@ public class MessageController {
 
 
     @GetMapping("/sent-by")
+    @PreAuthorize("hasAnyRole('Admin', 'Coordinator')")
     public ResponseEntity<List<SentMessageResponse>> getSenderMessages(@AuthenticationPrincipal User user) {
         if (user == null) {
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
