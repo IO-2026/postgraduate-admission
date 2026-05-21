53c8bef18f5f1e2a56c0f4215890735bf19c6c0d
mat32121 <140343869+mat32121@users.noreply.github.com>
2026-05-21 16:32:34 +0200

    Fix UserMapper field mapping for candidatePesel and candidateDateOfBirth

--- backend/src/main/java/com/example/backend/model/user/UserMapper.java
+++ backend/src/main/java/com/example/backend/model/user/UserMapper.java
@@ -36,8 +36,8 @@ public interface UserMapper {
     @Mapping(source = "user.id", target = "id")
     @Mapping(source = "application.id", target = "applicationId")
     @Mapping(source = "user.telNumber", target = "telNumber")
-    @Mapping(source = "application.applicantPesel", target = "pesel")
-    @Mapping(source = "application.applicantDateOfBirth", target = "dateOfBirth")
+    @Mapping(source = "application.candidatePesel", target = "pesel")
+    @Mapping(source = "application.candidateDateOfBirth", target = "dateOfBirth")
     @Mapping(source = "application.submissionDateTime", target = "submissionDateTime")
     CandidateWithApplicationDto toCandidateWithApplicationDto(User user, Application application);
 
